f220d18b472ea9bcf55612f1e9124ba15e71dc50
Gopinath <gopinath.manchineella@gmail.com>
2017-11-29 22:41:04 -0500

    router fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,7 +16,7 @@ const routes: Routes = [
 
   @NgModule({
     imports: [
-      RouterModule.forRoot(routes) //, { useHash: true }
+      RouterModule.forRoot(routes, { useHash: true }) //, { useHash: true }
     ],
     exports: [RouterModule]
   })
